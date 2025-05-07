e97f29d5525fa450928b504466bebd0850efc5cd
josephjose2243 <80537908+josephjose2243@users.noreply.github.com>
2025-05-07 22:55:32 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
-// config/db.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    // Connecting without the deprecated options
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (err) {
-    console.error("MongoDB connection error:", err.message);
+    console.error("❌ MongoDB connection error:", err.message);
     process.exit(1);
   }
 };
